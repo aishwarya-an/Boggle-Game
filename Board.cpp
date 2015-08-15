f613b2479bc62918a39becb0c28f0b0ad3fe8fcd
Aishwarya A N <aishwarya.an95@gmail.com>
2015-08-15 14:17:04 +0530

    added the implementation of the constructors and the destructor of the Board class.
    * There are two constructors : one taking the number of rows and columns of the board as arguments and the other being the copy constructor.
    * The destructor frees the memory dynamically allocated by the constructor.
    
    Signed-off-by: Aishwarya A N <aishwarya.an95@gmail.com>

--- Board.cpp
+++ Board.cpp
@@ -1,17 +1,40 @@
 // This is the declaration file of Board class
 
+#include <cstdlib>
+#include <ctime>
 #include "Board.h"
 
 using namespace std;
 
-// This is the constructor which takes in the number of rows and columns of the board to be constructed.
+// This is the constructor which takes in the number of rows and columns of the board to be constructed. The number of rows and
+// columns in the board should be atleast 4. So, if the number of rows and columns taken in is < 4, then the board is constructed
+// with the number of rows and columns to be 4. It then gets a random letter and puts the same into each block of the grid. Since,
+// there are only a few words starting with the letter 'Q' and most of those words start with "Qu", the board has "Qu" instead 
+// of only the letter 'Q'.
 Board::Board(int row, int column){
-
+	if(row < 4)
+		row = 4;
+	if(column < 4)
+		column = 4;
+	rows = row;
+	columns = column;
+	srand(time(NULL));
+	grid = new vector<string>(rows * columns, "");
+	int i = 0;
+	while(i < grid->size()){
+		(*grid)[i] = (rand() % 26) + 65;
+		if((*grid)[i] == "Q")
+			(*grid)[i] = "Qu";
+		++i;
+	}
 }
 
 // This is the copy constructor which takes in another object and copies its contents into another object.
 Board::Board(const Board &another){
-
+	const vector<string>* another_grid = another.get_board();
+	grid = new vector<string>(*another_grid);
+	rows = another.get_rows();
+	columns = get_columns();
 }
 
 // This function returns the number of rows in the grid.
@@ -36,5 +59,5 @@ void Board::print(){
 
 // This is the destructor which frees the memory dynamically allocated by the constructor.
 Board::~Board(){
-
+	delete grid;
 }

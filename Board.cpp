5d6335c0431d506c9d7e84709582cc054f14f14e
Aishwarya A N <aishwarya.an95@gmail.com>
2015-08-21 08:42:09 +0530

    changed the implementation of the constructor and added a new function get_number().
    * In the header file, added a structure containing the array of the frequency of the letters in the English alphabet.
    * Added a new function get_number() which is called by the constructor and returns a random number i where the ith letter will be inserted in the board. The returned value i has the probability p, where p is the frequency of the ith letter.
    * Since, the letters are inserted into the board using the frequencies of the letters, the probability of getting words from the board is more than that of the previous board where the letters where randomly inserted.
    
    Signed-off-by: Aishwarya A N <aishwarya.an95@gmail.com>

--- Board.cpp
+++ Board.cpp
@@ -8,9 +8,10 @@ using namespace std;
 
 // This is the constructor which takes in the number of rows and columns of the board to be constructed. The number of rows and
 // columns in the board should be atleast 4. So, if the number of rows and columns taken in is < 4, then the board is constructed
-// with the number of rows and columns to be 4. It then gets a random letter and puts the same into each block of the grid. Since,
-// there are only a few words starting with the letter 'Q' and most of those words start with "Qu", the board has "Qu" instead 
-// of only the letter 'Q'.
+// with the number of rows and columns to be 4. It then gets a letter according to the random number generated and puts the same
+//  into each block of the grid. Since there are only a few words starting with the letter 'Q' and most of those words start with
+//  "Qu", the board has "Qu" instead of only the letter 'Q'. Its running time is O(mn) where m and n are the number of rows and
+// columns in the board.
 Board::Board(int row, int column){
 	if(row < 4)
 		row = 4;
@@ -18,16 +19,34 @@ Board::Board(int row, int column){
 		column = 4;
 	rows = row;
 	columns = column;
-	srand(time(NULL));
 	grid = new vector<string>(rows * columns, "");
 	int i = 0;
+	srand(time(NULL));
 	while(i < grid->size()){
-		(*grid)[i] = (rand() % 26) + 65;
-		if((*grid)[i] == "Q")
-			(*grid)[i] = "Qu";
+		int letter = get_number();
+		(*grid)[i] = letter + 65;
 		++i;
 	}
 }
+
+
+// This is a private function which is called by the constructor. It returns a random number i which has the probability 
+// p, where p = frequencies[i]. The array frquencies[] is a private member containing the frquencies of the letters in the 
+// English alphabet.
+int Board::get_number(){
+	double sum = 0.0;
+    while(true){
+    	// Getting a random real value in the interval [0.0, 1.0).
+        double random = 1.00 * rand() / RAND_MAX;
+        sum = 0.0;        
+        for(int i = 0; i < 26; i++){
+            sum += letters.array[i];
+            if(sum > random) 
+            	return i;
+        }
+    }
+}
+
 
 // This is the copy constructor which takes in another object and copies its contents into another object.
 Board::Board(const Board &another){
@@ -37,22 +56,26 @@ Board::Board(const Board &another){
 	columns = get_columns();
 }
 
+
 // This function returns the number of rows in the grid.
 int Board::get_rows() const{
 	return rows;
 }
+
 
 // This function returns the number of columns in the board.
 int Board::get_columns() const{
 	return columns;
 }
 
+
 // This function returns the grid.
 const vector<string>* Board::get_board() const{
 	return grid;
 }
 
-// This function prints the board.
+
+// This function prints the board. Its running time is O(mn) where m and n are the number of rows and columns in the board.
 void Board::print(){
 	int i = 0;
 	while(i < rows){
@@ -83,6 +106,7 @@ void Board::print(){
 	}
 	cout << endl;
 }
+
 
 // This is the destructor which frees the memory dynamically allocated by the constructor.
 Board::~Board(){
--- Board.h
+++ Board.h
@@ -9,11 +9,23 @@ using namespace std;
 #ifndef BOARD_H
 #define BOARD_H
 
+struct frequencies{
+	double array[26] = {
+        0.08167, 0.01492, 0.02782, 0.04253, 0.12703, 0.02228,
+        0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025,
+        0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987,
+        0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150,
+        0.01974, 0.00074};
+};
+
 class Board{
 private:
 	vector<string>* grid;
 	int rows;
 	int columns;
+    // The frequencies of letters in the English alphabet
+   	frequencies letters;
+    int get_number();
 public:
 	Board(int, int);
 	Board(const Board&);

051fe56f1fe7e8503ef7664096950c4f2e005289
Aishwarya A N <aishwarya.an95@gmail.com>
2016-09-22 10:25:46 +0530

    added the main program of the graphical version of the Boggle Game.
    Signed-off-by: Aishwarya A N <aishwarya.an95@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "bogglesolver.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Bogglesolver w;
+    w.show();
+    return a.exec();
+}
